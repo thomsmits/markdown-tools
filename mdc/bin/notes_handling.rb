8f9975cd2bae8e045f2737cddca5be807a0ad4a8
Thomas Smits <thomsmits@gmail.com>
2014-07-16 18:48:04 +0200

    Added feature to create notes in markdown and render them to HTML.

---
+++ mdc/bin/notes_handling.rb
@@ -0,0 +1,139 @@
+# -*- coding: utf-8 -*-
+
+require_relative '../lib/parsing/properties_reader'
+require_relative '../lib/parsing/parser'
+require_relative '../lib/domain/presentation'
+require_relative '../lib/rendering/renderer_html_note'
+require 'date'
+require_relative '../lib/domain/notes'
+require 'fileutils'
+
+##
+# Generate a set of html files from a set of markdown notes
+class NotesHandling
+
+  ##
+  # Parse the given directory and generate html structure
+  # @param [String] source the source directory
+  # @param [String] target the target directory
+  def self.main(source, target)
+    handler = NotesHandling.new
+    root_folder = Notes::Folder.new(source, '', '', '')
+    handler.parse_folder(source, root_folder, true)
+    handler.read_files_and_convert(root_folder, target)
+  end
+
+  ##
+  # Convert an array in the form 'name: value', ... to a hash 'name' => 'value'
+  # @param [String[]] comments array with strings
+  # @return [Hash] a hash
+  def strings_to_hash(comments)
+    hash = {}
+    comments.each { |c|
+      if /(.*):(.*)/ =~ c
+        name = $1.strip
+        value = $2.strip
+        hash[name] = value
+      end
+    }
+    hash
+  end
+
+  ##
+  # @param [Folder] folder
+  # @param [String] target directory for the result files
+  def read_files_and_convert(folder, target)
+
+    p = Parsing::Parser.new
+
+    folder.files.each { |file|
+      src_path = folder.path + '/' + file.name;
+      dest_dir = target + '/' + folder.name;
+
+      FileUtils.mkdir_p(dest_dir)  unless Dir.exist?(dest_dir)
+
+      dest_path =  dest_dir + '/' + file.name + '.html'
+
+      pres = Domain::Presentation.new('', '', '', '', '', '', '', '', '')
+      p.parse(src_path, '', pres)
+      pres.title1 = pres.chapters[0].title
+      file.title = pres.title1
+      file.digest = pres.digest(180) + '...'
+      file.date = File.mtime(src_path)
+
+      metadata = strings_to_hash(pres.comments)
+
+      file.date = Date.parse(metadata['date'])  unless metadata['date'].nil?
+      file.tags = metadata['tags'].split(/, ?/)  unless metadata['tags'].nil?
+
+      io = File.new(dest_path, 'w', :encoding => 'UTF-8')
+      renderer = Rendering::RendererHTMLNote.new(io, '', '', '', '', file.tags, file.date, folder.title)
+      pres.render(renderer)
+      io.close
+    }
+
+    folder.folders.each { |dir|
+      read_files_and_convert(dir, target + '/' + folder.name)
+    }
+
+    dest_dir = target + '/' + folder.name;
+    make_index(dest_dir, folder)
+  end
+
+
+  def make_index(dest_dir, folder)
+
+    return  if folder.folders.count == 0 && folder.files.count == 0
+
+    Dir.mkdir(dest_dir)  unless Dir.exist?(dest_dir)
+    io = File.new(dest_dir + '/' + 'index.html', 'w')
+    renderer = Rendering::RendererHTMLNote.new(io, '', '', '', '', [], [], folder.title)
+    folder.render(renderer)
+    io.close
+  end
+
+  ##
+  # Recursively parse the given folder and return a tree containing all found data
+  # @param [String] directory of the root folder of the notes storage
+  # @param [String] folder the folder to add data to
+  # @return [Folder] root folder
+  def parse_folder(directory, folder, first = false)
+
+    # Unify to Unix file separators
+    directory.gsub!('\\', '/')
+
+    # Get last part of the path
+    directory =~ %r!.*/(.*?)$!
+    folder.name = first ? '' : $1
+
+
+    # Check for properties
+    if File.exist?("#{directory}/folder.properties")
+      props = Parsing::PropertiesReader.new("#{directory}/folder.properties")
+      folder.title = props.get('title')
+      folder.description = props.get('description')
+    else
+      folder.title = folder.name
+      folder.description = ''
+    end
+
+    # Search for files in folder
+    Dir.foreach(directory) { |d|
+
+      next  if d.end_with?('.')
+
+      path = "#{directory}/#{d}"
+
+      if File.file?(path) && (d.end_with?('.txt') || d.end_with?('.md'))
+        entry = Notes::FolderEntry.new(d)
+        folder << entry
+      elsif File.directory?(path)
+        sub_folder = Notes::Folder.new(path, '', '', '')
+        parse_folder(path, sub_folder)
+        folder.add_folder(sub_folder)
+      end
+    }
+  end
+end
+
+NotesHandling::main(ARGV[0], ARGV[1])
---
+++ mdc/lib/domain/notes.rb
@@ -0,0 +1,114 @@
+# -*- coding: utf-8 -*-
+
+module Notes
+
+  ##
+  # Class representing a folder containing notes
+  class Folder
+    attr_accessor :path, :name, :title, :description, :files, :folders
+
+    ##
+    # Create a new folder
+    # @param [String] path fully qualified path to the folder
+    # @param [String] name name of the folder
+    # @param [String] title title of the folder
+    # @param [String] description description of the
+    def initialize(path, name, title, description)
+      @path, @name, @title, @description, @files, @folders = path, name, title, description, [], []
+    end
+
+    ##
+    # Add a new entry to the folder
+    # @param [FolderEntry] entry entry to be added
+    def <<(entry)
+      @files << entry
+    end
+
+    ##
+    # Add sub folder to the folder
+    # @param [Folder] folder sub folder to be added
+    def add_folder(folder)
+      @folders << folder
+    end
+
+    ##
+    # Return a string representation
+    # @return [String] string representation
+    def to_s
+      result = ''
+      result << "#{@title} '#{@description}' -> #{@path}\n"
+
+      @files.each { |e|
+        result << '    ' << e.to_s << "\n"
+      }
+
+      result
+    end
+
+    ##
+    # Render contents
+    # @param [Rendering::RendererHTMLNote] renderer Rendering class used for generation
+    def render(renderer)
+      renderer.index_start(@title, @description)
+      @folders.each { |f| renderer.index_folder_entry(f.name, f.title, f.description, f.count, f.all_tags) }
+      @files.each { |f| f.render(renderer) }
+      renderer.index_end
+    end
+
+    ##
+    # Return the number of files in the folder
+    # @param [Fixnum] num already counted files for recursive calls
+    # @return [Fixnum] total number of files found
+    def count(num = 0)
+      num = @files.count
+      @folders.each { |f| num += f.count }
+      num
+    end
+
+    ##
+    # Return all tags (i.e. short terms describing the file's content)
+    # @return [String[]] found tags alphabetically sorted
+    def all_tags
+      tags = {}
+      @files.each { |f|
+        f.tags.each { |t|
+          tags[t] = :present
+        }
+      }
+
+      result = []
+      tags.each_key { |k|
+        result << k
+      }
+
+      result.sort!
+    end
+  end
+
+  ##
+  # Entry for a file of a folder
+  class FolderEntry
+    attr_accessor :name, :title, :digest, :date, :tags
+
+    ##
+    # Create a new instance
+    # @param [String] name name of the file
+    def initialize(name)
+      @name, @title, @digest, @tags = name, '', '', []
+    end
+
+    ##
+    # Return a string representation
+    # @return [String] string representation
+    def to_s
+      "#{@name} -> #{@title}"
+    end
+
+    ##
+    # Render contents
+    # @param [Rendering::RendererHTMLNote] renderer Rendering class used for generation
+    def render(renderer)
+      renderer.index_file_entry(@name, @title, @date, @tags, @digest)
+    end
+  end
+end
