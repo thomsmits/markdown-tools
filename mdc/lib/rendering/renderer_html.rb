thomsmits/markdown-tools

commit dd9979e945fe45cf4c6ff37cfddd7ba7017162dd

Rendering of LaTeX and HTML slides and files based on ERB templates.

mdc/lib/rendering/renderer_html.rb
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+require 'erb'
+
 require_relative 'renderer'
 require_relative '../messages'
 require_relative '../constants'
@@ -9,6 +11,221 @@ module Rendering
   ##
   # Base class for rendering slides to HTML
   class RendererHTML < Renderer
+
+    ## ERB templates to be used by the renderer
+    TEMPLATES = {
+        vertical_space: erb(
+            %q|
+            <br>
+            |
+        ),
+
+        equation: erb(
+            %q|
+            \[
+            <%= contents %>
+            \]
+            |
+        ),
+
+        ol_start: erb(
+            %q|
+            <ol start='<%= number %>'>
+            |
+        ),
+
+        ol_item: erb(
+            %q|
+            <li><%= inline_code(content) %>
+            |
+        ),
+
+        ol_end: erb(
+            %q|
+            </ol>
+            |
+        ),
+
+        ul_start: erb(
+            %q|
+            <ul>
+            |
+        ),
+
+        ul_item: erb(
+            %q|
+            <li><%= inline_code(content) %>
+            |
+        ),
+
+        ul_end: erb(
+            %q|
+            </ul>
+            |
+        ),
+
+        quote: erb(
+            %q|
+            <blockquote><%= inline_code(content) %>
+            <% if !source.nil? %>
+              <div class='quote_source'><%= source %></div>
+            <% end %>
+            </blockquote>
+            |
+        ),
+
+        important: erb(
+            %q|
+            <blockquote class='important'><%= inline_code(content) %>
+            </blockquote>
+            |
+        ),
+
+        question: erb(
+            %q|
+            <blockquote class='question'><%= inline_code(content) %>
+            </blockquote>
+            |
+        ),
+
+        script: erb(
+            %q|
+            <script><%= content %></script>
+            |
+        ),
+
+        code_start: erb(
+            %q|<pre><code class='<%= language %>' contenteditable>|
+        ),
+
+        code: erb(
+            %q|<%= entities(content) %>|
+        ),
+
+        code_end: erb(
+            %q|</code></pre>|
+        ),
+
+        table_start: erb(
+            %q|
+            <table class='small content'>
+            <thead><tr>
+            |
+        ),
+
+        table_end: erb(
+            %q|
+            </tbody></table>
+            |
+        ),
+
+
+        text: erb(
+            %q|
+            <p><%= inline_code(content) %></p>
+            |
+        ),
+
+        heading: erb(
+            %q|
+            <h<%= level %>><%= title %></h<%= level %>>
+            |
+        ),
+
+        toc_start: erb(
+            %q|
+            <section data-number='2'>
+            <h1 class='title toc'><%= LOCALIZED_MESSAGES[:toc] %></h1>
+            <ul>
+            |
+        ),
+
+        toc_entry: erb(
+            %q|
+            <li><a href='#<%= anchor %>'><%= name %></a>
+            |
+        ),
+
+        toc_end: erb(
+            %q|
+            </ul>
+            </section>
+            |
+        ),
+
+        toc_sub_entries_start: erb(
+            %q|
+            <ul class='subentry'>
+            |
+        ),
+
+        toc_sub_entry: erb(
+            %q|
+            <li><a href='#<%= anchor %>'><%= name %></a>
+            |
+        ),
+
+        toc_sub_entries_end: erb(
+            %q|
+            </ul>
+            |
+        ),
+
+        index_start: erb(
+            %q|
+            <!DOCTYPE html>
+            <html>
+            <head>
+              <meta charset='utf-8'>
+              <title><%= title1 %></title>
+              <%= include_javascript(INCLUDED_SCRIPTS) %>
+              <%= include_css(INCLUDED_STYLESHEETS) %>
+            </head>
+            <body>
+            <div class='title_first'><%= title1 %></div>
+            <div class='title_second'>%= title2 %></div>
+            <div class='copyright'>%= copyright%></div>
+            <div class='description'>%= description%></div>
+            <br>
+            <table>
+            <tr><th><%= LOCALIZED_MESSAGES[:chapter] %></th>
+            <th colspan='2'><%= LOCALIZED_MESSAGES[:material] %></th></tr>
+            |
+        ),
+
+        index_entry: erb(
+            %q|
+            <tr>
+            <td><%= chapter_number %> - <%= chapter_name %></td>
+            <td><a href='<%= slide_file %>'><%= slide_name %></a></td>
+            <td><a href='<%= plain_file %>'><%= plain_name %></a></td>
+            </tr>
+            |
+        ),
+
+        index_end: erb(
+            %q|
+            </table>
+            </div>
+            </body>
+            </html>
+            |
+        ),
+
+        html: erb(
+            %q|<%= content %>|
+        ),
+
+        css: erb(
+            %q|<link rel='stylesheet' href='<%= css %>'>
+            |
+        ),
+
+        js: erb(
+            %q|<script src='<%= js %>'></script>
+            |
+        ),
+    }
 
     CSS_PLAIN     = 'css/plain.css'
     CSS_BOOK      = 'css/book.css'
@@ -165,30 +382,28 @@ module Rendering
     ##
     # Vertical space
     def vertical_space
-      @io << '<br>' << nl
+      @io << TEMPLATES[:vertical_space].result(binding)
     end
 
     ##
     # Equation
     # @param [String] contents LaTeX source of equation
     def equation(contents)
-      @io << '\[' << nl << "#{contents}" << nl << '\]' << nl
+      @io << TEMPLATES[:equation].result(binding)
     end
 
     ##
     # Start of an ordered list
     # @param [Fixnum] number start number of list
     def ol_start(number = 1)
-      indent(@ul_level * 2)
-      @io << "<ol start='#{number}'>"
+      @io << TEMPLATES[:ol_start].result(binding)
       @ul_level += 1
     end
 
     ##
     # End of ordered list
     def ol_end
-      indent(@ul_level * 2)
-      @io << '</ol>'
+      @io << TEMPLATES[:ol_end].result(binding)
       @ul_level -= 1
     end
 
@@ -196,22 +411,20 @@ module Rendering
     # Item of an ordered list
     # @param [String] content content
     def ol_item(content)
-      ul_item(content)
+      @io << TEMPLATES[:ol_item].result(binding)
     end
 
     ##
     # Start of an unordered list
     def ul_start
-      indent(@ul_level * 2)
-      @io << "<ul>\n"
+      @io << TEMPLATES[:ul_start].result(binding)
       @ul_level += 1
     end
 
     ##
     # End of an unordered list
     def ul_end
-      indent(@ul_level * 2)
-      @io << "</ul>\n"
+      @io << TEMPLATES[:ul_end].result(binding)
       @ul_level -= 1
     end
 
@@ -219,9 +432,7 @@ module Rendering
     # Item of an unordered list
     # @param [String] content content
     def ul_item(content)
-      text = inline_code(content)
-      indent(@ul_level)
-      @io << "<li>#{text}\n"
+      @io << TEMPLATES[:ul_item].result(binding)
     end
 
     ##
@@ -229,32 +440,28 @@ module Rendering
     # @param [String] content the content
     # @param [String] source the source of the quote
     def quote(content, source)
-      @io << "<blockquote>#{inline_code(content)}" << nl
-      @io << "<div class='quote_source'>#{source}</div>" << nl  unless source.nil?
-      @io << '</blockquote>' << nl
+      @io << TEMPLATES[:quote].result(binding)
     end
 
     ##
     # Important
     # @param [String] content the box
     def important(content)
-      @io << "<blockquote class='important'>#{inline_code(content)}" << nl
-      @io << '</blockquote>' << nl
+      @io << TEMPLATES[:important].result(binding)
     end
 
     ##
     # Question
     # @param [String] content the box
     def question(content)
-      @io << "<blockquote class='question'>#{inline_code(content)}" << nl
-      @io << '</blockquote>' << nl
+      @io << TEMPLATES[:question].result(binding)
     end
 
     ##
     # Script
     # @param [String] content the script to be included
     def script(content)
-      @io << "<script>#{content}</script>" << nl
+      @io << TEMPLATES[:script].result(binding)
     end
 
     ##
@@ -262,21 +469,21 @@ module Rendering
     # @param [String] language language of the code fragment
     # @param [String] caption caption of the sourcecode
     def code_start(language, caption)
-      @io << "<pre><code class='#{language}' contenteditable>"
+      @io << TEMPLATES[:code_start].result(binding)
     end
 
     ##
     # End of a code fragment
     # @param [String] caption caption of the sourcecode
     def code_end(caption)
-      @io << '</code></pre>'
+      @io << TEMPLATES[:code_end].result(binding)
     end
 
     ##
     # Output code
     # @param [String] content the code content
     def code(content)
-      @io << entities(content)
+      @io << TEMPLATES[:code].result(binding)
     end
 
     ##
@@ -297,8 +504,8 @@ end
     # @param [Array] headers the headers
     # @param [Array] alignment alignments of the cells
     def table_start(headers, alignment)
-      @io << "<table class='small content'>" << nl
-      @io << '<thead><tr>' << nl
+
+      @io << TEMPLATES[:table_start].result(binding)
 
       headers.each_with_index do |e, i|
 
@@ -331,14 +538,14 @@ end
     ##
     # End of the table
     def table_end
-      @io << '</tbody></table>' << nl
+      @io << TEMPLATES[:table_end].result(binding)
     end
 
     ##
     # Simple text
     # @param [String] content the text
     def text(content)
-      @io << "<p>#{inline_code(content)}</p>" << nl
+      @io << TEMPLATES[:text].result(binding)
     end
 
     ##
@@ -346,27 +553,25 @@ end
     # @param [Fixnum] level heading level
     # @param [String] title title of the heading
     def heading(level, title)
-      @io << "<h#{level}>#{title}</h#{level}>" << nl
+      @io << TEMPLATES[:heading].result(binding)
     end
 
     ##
     # Start of the TOC
     def toc_start
-      @io << "<section data-number='2'>" << nl
-      @io << "  <h1 class='title toc'>#{LOCALIZED_MESSAGES[:toc]}</h1>" << nl
-      @io << '  <ul>' << nl
+      @io << TEMPLATES[:toc_start].result(binding)
     end
 
     ##
     # Start of sub entries in toc
     def toc_sub_entries_start
-      @io << "    <ul class='subentry'>" << nl
+      @io << TEMPLATES[:toc_sub_entries_start].result(binding)
     end
 
     ##
     # End of sub entries
     def toc_sub_entries_end
-      @io <<  '    </ul>' << nl
+      @io << TEMPLATES[:toc_sub_entries_end].result(binding)
     end
 
     ##
@@ -376,7 +581,7 @@ end
     def toc_sub_entry(name, anchor)
       return  if name == @last_toc_name
       @last_toc_name = name
-      @io << "      <li><a href='##{anchor}'>#{name}</a>" << nl
+      @io << TEMPLATES[:toc_sub_entry].result(binding)
     end
 
     ##
@@ -384,13 +589,13 @@ end
     # @param [String] name name of the entry
     # @param [String] anchor anchor of the entry
     def toc_entry(name, anchor)
-      @io << "    <li><a href='##{anchor}'>#{name}</a>" << nl
+      @io << TEMPLATES[:toc_entry].result(binding)
     end
 
     ##
     # End of toc
     def toc_end
-      @io << '  </ul>' << nl << '</section>' << nl
+      @io << TEMPLATES[:toc_end].result(binding)
     end
 
     ##
@@ -400,37 +605,13 @@ end
     # @param [String] copyright copyright info
     # @param [String] description description
     def index_start(title1, title2, copyright, description)
-      @io << <<-ENDOFTEXT
-      <!DOCTYPE html>
-      <html>
-      <head>
-        <meta charset='utf-8'>
-        <title>#{title1}</title>
-      #{include_javascript(INCLUDED_SCRIPTS)}
-      #{include_css(INCLUDED_STYLESHEETS)}
-      </head>
-
-      <body>
-        <div class='title_first'>#{title1}</div>
-        <div class='title_second'>#{title2}</div>
-        <div class='copyright'>#{copyright}</div>
-        <div class='description'>#{description}</div>
-        <br>
-      <table>
-      <tr><th>#{LOCALIZED_MESSAGES[:chapter]}</th>
-      <th colspan='2'>#{LOCALIZED_MESSAGES[:material]}</th></tr>
-      ENDOFTEXT
+      @io << TEMPLATES[:index_start].result(binding)
     end
 
     ##
     # End of index
     def index_end
-      @io << <<-ENDOFTEXT
-      </table>
-      </div>
-      </body>
-      </html>
-      ENDOFTEXT
+      @io << TEMPLATES[:index_end].result(binding)
     end
 
     ##
@@ -440,16 +621,14 @@ end
     # @param [String] slide_file file containing the slides
     # @param [String] plain_file file containing the plain version
     def index_entry(chapter_number, chapter_name, slide_file, slide_name, plain_file, plain_name)
-      @io << "<tr><td>#{chapter_number} - #{chapter_name}</td>"
-      @io << "<td><a href='#{slide_file}'>#{slide_name}</a></td>"
-      @io << "<td><a href='#{plain_file}'>#{plain_name}</a></td></tr>" << nl
+      @io << TEMPLATES[:index_entry].result(binding)
     end
 
     ##
     # HTML output
     # @param [String] content html
     def html(content)
-      @io << content << nl
+      @io << TEMPLATES[:html].result(binding)
     end
 
     ##
@@ -476,14 +655,14 @@ end
     # Single css entry
     # @param [String] css location of css file
     def css(css)
-      "<link rel='stylesheet' href='#{css}'>" << nl
+      TEMPLATES[:css].result(binding)
     end
 
     ##
     # Single JavaScript entry
     # @param [String] js location of JavaScript file
     def js(js)
-      "<script src='#{js}'></script>" << nl
+      TEMPLATES[:js].result(binding)
     end
 
     ##

mdc/lib/rendering/renderer_html_presentation.rb
@@ -9,6 +9,182 @@ module Rendering
   # Renderer to HTML for presentations
   class RendererHTMLPresentation < RendererHTML
 
+    ## ERB templates to be used by the renderer
+    TEMPLATES = {
+        button: erb(
+            %q|
+           <button onclick='executeNew(this);' class='runbutton'>
+           <%= LOCALIZED_MESSAGES[:run] %>
+           </button><span style='display: none;' name='log' class='output'>&nbsp;</span>
+           |
+        ),
+
+        button_with_log: erb(
+            %q|
+            <button onclick='executeNew(this);' class='runbutton'>
+            <%= LOCALIZED_MESSAGES[:run] %>
+            </button><div name='log' class='output_small'></div>
+           |
+        ),
+
+        button_with_log_pre: erb(
+            %q|
+            <button onclick='executePre(this);' class='runbutton'>
+            <%= LOCALIZED_MESSAGES[:run] %>
+            </button><div name='log' class='output_small'></div>
+           |
+        ),
+
+        link_previous: erb(
+            %q|
+            <div class='outputhtml plain' id='<%= line_id %>' name='html_output'>&nbsp;</div>
+            <script>attachPreviousHandler($('#<%= line_id %>'));</script>
+           |
+        ),
+
+        live_css: erb(
+            %q|
+            <iframe name='dest' src='' class='framed_wide'></iframe>
+            <script id='<%= line_id %>'>attachHandlerCSS($('#<%= line_id %>'), <%= fragment %>);</script>
+           |
+        ),
+
+        live_preview: erb(
+            %q|
+            <div class='outputhtml plain' id='<%= line_id %>' name='html_output'>&nbsp;</div>
+            <script>attachHandler($('#<%= line_id %>'));</script>
+           |
+        ),
+
+        live_preview_float: erb(
+            %q|
+            <div class='outputhtml' style='float: right;' id='<%= line_id %>' name='html_output'>&nbsp;</div>
+            <script>synchronize($('#<%= line_id %>'));</script>
+           |
+        ),
+
+        comment_start: erb(
+            %q|
+            <div class='more'><img src='img/help.png' onclick="$('#dialog_<%= @dialog_counter %>').dialog('open')"></div>
+            <div id='dialog_<%= @dialog_counter %>' title='<%= LOCALIZED_MESSAGES[:more_info] %>'><p>
+            |
+        ),
+
+        comment_end: erb(
+            %q|
+            <p></div>
+            <script>$('#dialog_<%= @dialog_counter %>').dialog( { width: 900, autoOpen: false  %> );</script>
+            |
+        ),
+
+        image: erb(
+            %q|
+            <img class='presentation' src='<%= chosen_image %>' alt='<%= alt %>' title='<%= title %>'<%= width_attr %>>
+            <div class='img_info'><%= inline(title) %></div>
+            |
+        ),
+
+        uml: erb(
+            %q|
+            <img src='<%= img_path %>' width='<%= width_slide %>'>
+            |
+        ),
+
+        chapter_start: erb(
+            %q|
+            <section id='<%= id %>' class='chapter' data-number='<%= number %>'>
+            <h1 class='trenner'><%= title %></h1>
+            </section>
+           |
+        ),
+
+        chapter_end: erb(
+            %q|
+            |
+        ),
+
+        slide_start: erb(
+            %q|
+            <section id='<%= id %>' class='slide' data-number='<%= number %>'>
+            <h2 class='title'><%= inline_code(title) %></h2>
+            |
+        ),
+
+        slide_end: erb(
+            %q|
+            </section>
+            |
+        ),
+
+        presentation_start: erb(
+            %q|
+            <!DOCTYPE html>
+            <html lang='de'>
+
+            <head>
+              <meta charset='utf-8'>
+              <title><%= title1 %>: <%= section_name %></title>
+              <meta name='author' content='<%= author %>'>
+              <meta name='apple-mobile-web-app-capable' content='yes' />
+              <meta name='apple-mobile-web-app-status-bar-style' content='black-translucent' />
+              <%= include_css(INCLUDED_STYLESHEETS) %>
+              <%= include_javascript(INCLUDED_SCRIPTS_HEAD) %>
+            </head>
+
+            <body>
+            <div class='reveal'>
+            <!-- Used to fade in a background when a specific slide state is reached -->
+            <div class='state-background'></div>
+
+            <!-- Any section element inside of this container is displayed as a slide -->
+            <div class='slides'>
+
+            <section data-number=''>
+              <h1><%= title1 %></h1>
+              <h2><%= title2 %></h2>
+              <div class='kapitel_nr' style='margin-top: 50%'><%= section_number %></div>
+              <div class='kapitel'><%= section_name %></div>
+              <img class='plain' style='position: absolute; bottom: -10px; right: 0;' src='img/logo_title.png' alt='Telefon'>
+            </section>
+            |
+        ),
+
+        presentation_end: erb(
+            %q?
+            </div>
+
+            <!-- The navigational controls UI -->
+            <aside class='controls'>
+              <a class='left' href='#'>&#x25C4;</a>
+            <a class='right' href='#'>&#x25BA;</a>
+            <a class='up' href='#'>&#x25B2;</a>
+            <a class='down' href='#'>&#x25BC;</a>
+            </aside>
+
+            <!-- Presentation progress bar -->
+            <div class='progress'><span></span></div>
+
+            </div>
+            <div class='menu'>
+            <ul id='menu'>
+            <li id='menu-inhalt'><%= LOCALIZED_MESSAGES[:contents] %>
+            <%= toc_menu %>
+            </ul>
+            </div>
+            <div class='copyright'>
+            <%= title1 %> | <%= copyright %>
+            </div>
+              <div class='nummer'>
+              <span id='slide_nr'>&nbsp;</span>
+            </div>
+              <%= include_javascript(INCLUDED_SCRIPTS_FOOTER) %>
+                <%= scripts(JAVASCRIPTS) %>
+              </body>
+            </html>
+            ?
+        ),
+    }
+
     ##
     # Stylesheets used by this renderer
     INCLUDED_STYLESHEETS = [
@@ -68,11 +244,7 @@ module Rendering
     # Render a button
     # @param [String] line_id internal ID of the line
     def button(line_id)
-      @io << <<-ENDOFTEXT
-      <button onclick='executeNew(this);' class='runbutton'>
-      #{LOCALIZED_MESSAGES[:run]}
-      </button><span style='display: none;' name='log' class='output'>&nbsp;</span>
-      ENDOFTEXT
+      @io << TEMPLATES[:button].result(binding)
     end
 
     ##
@@ -81,48 +253,33 @@ module Rendering
     # @param [String] number the number of the chapter
     # @param [String] id the uniquie id of the chapter (for references)
     def chapter_start(title, number, id)
-      @io << <<-ENDOFTEXT
-      <section id='#{id}' class='chapter' data-number='#{number}'>
-        <h1 class='trenner'>#{title}</h1>
-      </section>
-      ENDOFTEXT
+      @io << TEMPLATES[:chapter_start].result(binding)
     end
 
     ## End of a chapter
     def chapter_end
-      @io << nl
+      @io << TEMPLATES[:chapter_end].result(binding)
     end
 
     ##
     # Render a button with log area
     # @param [String] line_id internal ID of the line
     def button_with_log(line_id)
-      @io << <<-ENDOFTEXT
-      <button onclick='executeNew(this);' class='runbutton'>
-      #{LOCALIZED_MESSAGES[:run]}
-      </button><div name='log' class='output_small'></div>
-      ENDOFTEXT
+      @io << TEMPLATES[:button_with_log].result(binding)
     end
 
     ##
     # Render a button with output
     # @param [String] line_id internal ID of the line
     def button_with_log_pre(line_id)
-      @io << <<-ENDOFTEXT
-      <button onclick='executePre(this);' class='runbutton'>
-      #{LOCALIZED_MESSAGES[:run]}
-      </button><div name='log' class='output_small'></div>
-      ENDOFTEXT
+      @io << TEMPLATES[:button_with_log_pre].result(binding)
     end
 
     ##
     # Link to previous slide (for active HTML)
     # @param [String] line_id internal ID of the line
     def link_previous(line_id)
-      @io << <<-ENDOFTEXT
-      <div class='outputhtml plain' id='#{line_id}' name='html_output'>&nbsp;</div>
-      <script>attachPreviousHandler($('##{line_id}'));</script>
-      ENDOFTEXT
+      @io << TEMPLATES[:link_previous].result(binding)
     end
 
     ##
@@ -130,48 +287,33 @@ module Rendering
     # @param [String] line_id internal ID of the line
     # @param [String] fragment HTML fragment used for CSS styling
     def live_css(line_id, fragment)
-      @io << <<-ENDOFTEXT
-      <iframe name='dest' src='' class='framed_wide'></iframe>
-      <script id='#{line_id}'>attachHandlerCSS($('##{line_id}'), #{fragment});</script>
-      ENDOFTEXT
+      @io << TEMPLATES[:live_css].result(binding)
     end
 
     ##
     # Link to previous slide (for active CSS)
     # @param [String] line_id internal ID of the line
     def live_preview(line_id)
-      @io << <<-ENDOFTEXT
-      <div class='outputhtml plain' id='#{line_id}' name='html_output'>&nbsp;</div>
-      <script>attachHandler($('##{line_id}'));</script>
-      ENDOFTEXT
+      @io << TEMPLATES[:live_preview].result(binding)
     end
 
     ##
     # Perform a live preview
     # @param [String] line_id internal ID of the line
     def live_preview_float(line_id)
-      @io << <<-ENDOFTEXT
-      <div class='outputhtml' style='float: right;' id='#{line_id}' name='html_output'>&nbsp;</div>
-      <script>synchronize($('##{line_id}'));</script>
-      ENDOFTEXT
+      @io << TEMPLATES[:live_preview_float].result(binding)
     end
 
     ##
     # Beginning of a comment section, i.e. explanations to the current slide
     def comment_start
-      @io << <<-ENDOFTEXT
-      <div class='more'><img src='img/help.png' onclick="$('#dialog_#{@dialog_counter}').dialog('open')"></div>
-      <div id='dialog_#{@dialog_counter}' title='#{LOCALIZED_MESSAGES[:more_info]}'><p>
-      ENDOFTEXT
+      @io << TEMPLATES[:comment_start].result(binding)
     end
 
     ##
     # End of comment section
     def comment_end
-      @io << <<-ENDOFTEXT
-      <p></div>
-      <script>$('#dialog_#{@dialog_counter}').dialog( { width: 900, autoOpen: false } );</script>
-      ENDOFTEXT
+      @io << TEMPLATES[:comment_end].result(binding)
 
       @dialog_counter += 1
     end
@@ -195,8 +337,7 @@ module Rendering
           width_attr = " width='#{width_slide}'"
       end
 
-      @io << "<img class='presentation' src='#{chosen_image}' alt='#{alt}' title='#{title}'#{width_attr}>" << nl
-      @io << "<div class='img_info'>#{inline(title)}</div>" << nl
+      @io << TEMPLATES[:image].result(binding)
     end
 
     ##
@@ -207,7 +348,7 @@ module Rendering
     # @param [String] width_plain width of the diagram on plain documents
     def uml(picture_name, contents, width_slide, width_plain)
       img_path = super(picture_name, contents, width_slide, width_plain, 'svg')
-      @io << "<img src='#{img_path}' width='#{width_slide}'>" << nl
+      @io << TEMPLATES[:uml].result(binding)
     end
 
     ##
@@ -221,36 +362,7 @@ module Rendering
     # @param [String] description additional description
     # @param [String] term of the lecture
     def presentation_start(title1, title2, section_number, section_name, copyright, author, description, term = '')
-      @io << <<-ENDOFTEXT
-      <!DOCTYPE html>
-      <html lang='de'>
-
-      <head>
-        <meta charset='utf-8'>
-        <title>#{title1}: #{section_name}</title>
-        <meta name='author' content='#{author}'>
-        <meta name='apple-mobile-web-app-capable' content='yes' />
-        <meta name='apple-mobile-web-app-status-bar-style' content='black-translucent' />
-        #{include_css(INCLUDED_STYLESHEETS)}
-        #{include_javascript(INCLUDED_SCRIPTS_HEAD)}
-      </head>
-
-      <body>
-      <div class='reveal'>
-      <!-- Used to fade in a background when a specific slide state is reached -->
-      <div class='state-background'></div>
-
-      <!-- Any section element inside of this container is displayed as a slide -->
-      <div class='slides'>
-
-      <section data-number=''>
-        <h1>#{title1}</h1>
-        <h2>#{title2}</h2>
-        <div class='kapitel_nr' style='margin-top: 50%'>#{section_number}</div>
-        <div class='kapitel'>#{section_name}</div>
-        <img class='plain' style='position: absolute; bottom: -10px; right: 0;' src='img/logo_title.png' alt='Telefon'>
-      </section>
-      ENDOFTEXT
+      @io << TEMPLATES[:presentation_start].result(binding)
     end
 
     ##
@@ -262,38 +374,7 @@ module Rendering
     # @param [String] copyright copyright information
     # @param [String] author author of the presentation
     def presentation_end(title1, title2, section_number, section_name, copyright, author)
-      @io << <<-ENDOFTEXT
-      </div>
-
-      <!-- The navigational controls UI -->
-      <aside class='controls'>
-        <a class='left' href='#'>&#x25C4;</a>
-      <a class='right' href='#'>&#x25BA;</a>
-      <a class='up' href='#'>&#x25B2;</a>
-      <a class='down' href='#'>&#x25BC;</a>
-      </aside>
-
-      <!-- Presentation progress bar -->
-      <div class='progress'><span></span></div>
-
-      </div>
-      <div class='menu'>
-      <ul id='menu'>
-      <li id='menu-inhalt'>#{LOCALIZED_MESSAGES[:contents]}
-      #{toc_menu}
-      </ul>
-      </div>
-      <div class='copyright'>
-      #{title1} | #{copyright}
-      </div>
-      <div class='nummer'>
-      <span id='slide_nr'>&nbsp;</span>
-      </div>
-      #{include_javascript(INCLUDED_SCRIPTS_FOOTER)}
-      #{scripts(JAVASCRIPTS)}
-      </body>
-      </html>
-      ENDOFTEXT
+      @io << TEMPLATES[:presentation_end].result(binding)
     end
 
     ##
@@ -304,7 +385,7 @@ module Rendering
 
       result << '<ul>' << nl
 
-      @toc.entries.each { |e|
+      @toc.entries.each do |e|
         result << "  <li><a href='##{e.id}'>#{e.name}</a>" << nl
 
         if e.entries.length > 0
@@ -319,7 +400,7 @@ module Rendering
         end
 
         result << '    </ul>' << nl
-      }
+      end
 
       result << '    </ul>' << nl
       result
@@ -332,16 +413,13 @@ module Rendering
     # @param [String] id the unique id of the slide (for references)
     # @param [Boolean] contains_code indicates whether the slide contains code fragments
     def slide_start(title, number, id, contains_code)
-      @io << <<-ENDOFTEXT
-      <section id='#{id}' class='slide' data-number='#{number}'>
-        <h2 class='title'>#{inline_code(title)}</h2>
-      ENDOFTEXT
+      @io << TEMPLATES[:slide_start].result(binding)
     end
 
     ##
     # End of slide
     def slide_end
-      @io << '</section>' << nl
+      @io << TEMPLATES[:slide_end].result(binding)
     end
   end
 end

commit 9c8f6ed006bafcf0e15ac16433ae5a90f6a87d2e

Fixed errors in HTML renderer introduced with commit: dd9979e945.

mdc/lib/rendering/renderer_html.rb
@@ -191,9 +191,9 @@ module Rendering
             </head>
             <body>
             <div class='title_first'><%= title1 %></div>
-            <div class='title_second'>%= title2 %></div>
-            <div class='copyright'>%= copyright%></div>
-            <div class='description'>%= description%></div>
+            <div class='title_second'><%= title2 %></div>
+            <div class='copyright'><%= copyright%></div>
+            <div class='description'><%= description%></div>
             <br>
             <table>
             <tr><th><%= LOCALIZED_MESSAGES[:chapter] %></th>

mdc/lib/rendering/renderer_html_presentation.rb
@@ -73,7 +73,7 @@ module Rendering
         comment_end: erb(
             %q|
             <p></div>
-            <script>$('#dialog_<%= @dialog_counter %>').dialog( { width: 900, autoOpen: false  %> );</script>
+            <script>$('#dialog_<%= @dialog_counter %>').dialog( { width: 900, autoOpen: false  } );</script>
             |
         ),
 