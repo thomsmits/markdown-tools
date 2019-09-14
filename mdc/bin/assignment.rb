6dcc2e9b2ad8c831773138da214b99759c3ddbf1
Thomas Smits <thomsmits@gmail.com>
2019-09-14 02:16:41 +0200

    Added another parsing and rendering path for my assignments (not of general interest)

---
+++ mdc/bin/assignment.rb
@@ -0,0 +1,64 @@
+require 'stringio'
+
+require_relative 'custom_handler'
+require_relative '../lib/parsing/properties_reader'
+require_relative '../lib/parsing/parser'
+require_relative '../lib/domain/presentation'
+require_relative '../lib/rendering/renderer_latex_assignment'
+
+##
+# Create TeX snippets form Markdown files for assignments.
+# This covers a very special use case. For more generic use cases
+# use the fle `main.rb`
+class Assignment
+  ##
+  # Parse lines containing a markdown file and return the rendered result.
+  # @param src_dir String directory with source files
+  # @param dest_dir String target directory
+  # @param prog_language String the default programming language
+  # @param renderer_class String name of class used for rendering
+  # @param lines String[] text to be parsed
+  # @return String the rendered contents as
+  def self.parse_file_and_render(src_dir, dest_dir, prog_language, lines)
+    CustomHandler.convert_stream(src_dir, dest_dir, prog_language,
+                                 'Rendering::RendererLatexAssignment', lines)
+  end
+
+  ##
+  # Parse a whole directory of files.
+  # @param src_dir String directory with source files
+  # @param dest_dir String directory to store results in
+  # @param language String default programming language
+  def self.parse_directory_and_render(src_dir, dest_dir, language = '')
+
+    # Get all files in the directory
+    files = Dir.new(src_dir).entries.select { |f| (/^.*\.md$/ =~ f) }
+
+    files.each do |file|
+      target_name = file.gsub('.md', '.tex')
+      CustomHandler.convert_file(src_dir, dest_dir, file, target_name, language,
+                                 'Rendering::RendererLatexAssignment')
+    end
+  end
+end
+
+if $0 == __FILE__
+  # Get and remove command line arguments
+  src_dir = ARGV.shift
+  dest_dir = ARGV.shift
+
+  if ARGV.size.zero?
+    # Only directories given. Compile whole directory
+    puts Assignment.parse_directory_and_render(src_dir, dest_dir)
+  else
+    # More data given, compile single file
+    prog_language = ARGV.shift
+
+    # With an empty ARGV, ARGF will read from STDIN
+    # otherwise it will open all files ARGV is containing anc
+    #  concatenate them
+    lines = ARGF.readlines
+
+    puts Assignment.parse_file_and_render(src_dir, dest_dir, prog_language, lines)
+  end
+end
---
+++ mdc/lib/rendering/renderer_latex_assignment.rb
@@ -0,0 +1,150 @@
+require 'erb'
+
+require_relative 'renderer_latex'
+require_relative '../messages'
+
+module Rendering
+  ##
+  # Render the presentation into a latex file for further processing
+  # using LaTeX
+  class RendererLatexAssignment < RendererLatex
+    ## ERB templates to be used by the renderer
+    TEMPLATES = {
+      presentation_start: erb(
+        ''
+      ),
+
+      presentation_end: erb(
+        ''
+      ),
+
+      chapter_start: erb(
+        ''
+      ),
+
+      chapter_end: erb(
+        ''
+      ),
+
+      slide_start: erb(
+        %q(
+        % ----------------------------------------------------------------------------
+        \subsection{<%= inline_code(title) %>}\label{<%= id %>}
+        )
+      ),
+
+      slide_end: erb(
+        ''
+      ),
+
+      comment_start: erb(
+        %q(\begin{solution}
+        )
+      ),
+
+      comment_end: erb(
+        %q(\end{solution}
+        )
+      ),
+
+      text: erb(
+        '
+        <%= inline_code(content) %>
+        '
+      ),
+
+      code_start: erb(
+        %q(
+        \vspace{-2mm}
+        \begin{lstlisting}[language=<%= language %>,<%= caption_command %><%= column_style %>basicstyle=\small\ttfamily])
+      ),
+
+      code: erb(
+        '<%= content %>'
+      ),
+
+      code_end: erb(
+        '\end{lstlisting}\vspace{2mm}' + "\n"
+      ),
+
+      multiple_choice_start: erb(
+          '\begin{itemize}'
+        ),
+
+      multiple_choice_end: erb(
+          '\end{itemize}'
+        ),
+
+      multiple_choice: erb(
+          %q(\item <%= if correct then '[X]' else '[ ]' end %> <%= text %>)
+        )
+    }.freeze
+
+    ##
+    # Initialize the renderer
+    # @param [IO] io target of output operations
+    # @param [String] language the default language for code snippets
+    # @param [String] result_dir location for results
+    # @param [String] image_dir location for generated images (relative to result_dir)
+    # @param [String] temp_dir location for temporary files
+    def initialize(io, language, result_dir, image_dir, temp_dir)
+      super(io, language, result_dir, image_dir, temp_dir)
+    end
+
+    ##
+    # Method returning the templates used by the renderer. Should be overwritten by the
+    # subclasses.
+    # @return [Hash] the templates
+    def all_templates
+      @templates = super.merge(TEMPLATES)
+    end
+
+    ##
+    # Indicates whether the renderer handles animations or not. false indicates
+    # that slides should not be repeated.
+    # @return [Boolean] +true+ if animations are supported, otherwise +false+
+    def handles_animation?
+      false
+    end
+
+    ##
+    # Start of slide
+    # @param [String] title the title of the slide
+    # @param [String] number the number of the slide
+    # @param [String] id the unique id of the slide (for references)
+    # @param [Boolean] contains_code indicates whether the slide contains code fragments
+    def slide_start(title, number, id, contains_code)
+      super unless title == @last_title
+    end
+
+    ##
+    # Render an image
+    # @param [String] location path to image
+    # @param [Array] _formats available file formats
+    # @param [String] _alt alt text
+    # @param [String] title title of image
+    # @param [String] width_slide width for slide
+    # @param [String] source source of the image
+    def image(location, _formats, _alt, title, width_slide, width_plain, source = nil)
+      width = width_plain || width_slide
+
+      # Skip images with width 0
+      unless /^0$/ === width_plain || /^0%$/ === width_plain
+        image_latex(location, title, width, source)
+      end
+    end
+
+    ##
+    # Render an UML inline diagram using an external tool
+    # @param [String] picture_name name of the picture
+    # @param [String] contents the embedded UML
+    # @param [String] width_slide width of the diagram on slides
+    # @param [String] width_plain width of the diagram on plain documents
+    def uml(picture_name, contents, width_slide, width_plain)
+      new_width = calculate_width(width_plain)
+      formats = %w[pdf eps]
+      img_path = super(picture_name, contents, width_slide, width_plain, 'pdf')
+      image(img_path, formats, '', '', new_width, new_width)
+    end
+  end
+end
