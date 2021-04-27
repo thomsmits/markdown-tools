8ac1142302777977acf3a1c65f09e0ec78eebb3b
Thomas Smits <thomsmits@gmail.com>
2021-04-27 23:16:33 +0200

    Introduced modules and deleted unused files

--- mdc/lib/rendering/line_renderer.rb
+++ mdc/lib/rendering/line_renderer.rb
@@ -1,64 +1,69 @@
+module Rendering
 
+  ##
+  # Default implementation of the line renderer.
+  # This class does nothing but return the Markdown
+  # code.
+  class LineRenderer
 
-class LineRenderer
-
-  def render_text(content)
-    content
-  end
-
-  def render_code(content)
-    "`#{content}`"
-  end
-
-  def render_strongunderscore(content)
-    "__#{content}__"
-  end
-
-  def render_strongstar(content)
-    "**#{content}**"
-  end
-
-  def render_emphasisunderscore(content)
-    "_#{content}_"
-  end
-
-  def render_emphasisstar(content)
-    "*#{content}*"
-  end
-
-  def render_superscript(content)
-    "^#{content}"
-  end
-
-  def render_subscript(content)
-    "_#{content}"
-  end
-
-  def render_link(content, target, title)
-    if node.title.nil?
-      %Q{[#{content}](#{target})}
-    else
-      %Q{[#{content}](#{target} "#{title}")}
+    def render_text(content)
+      content
     end
-  end
 
-  def render_reflink(content)
-    # TODO: Implement
-  end
+    def render_code(content)
+      "`#{content}`"
+    end
 
-  def render_formula(content)
-    "\[ #{content} \]"
-  end
+    def render_strongunderscore(content)
+      "__#{content}__"
+    end
 
-  def render_deleted(content)
-    "~~#{content}~~"
-  end
+    def render_strongstar(content)
+      "**#{content}**"
+    end
 
-  def render_underline(content)
-    "~#{content}~"
-  end
+    def render_emphasisunderscore(content)
+      "_#{content}_"
+    end
 
-  def render_unparsed(content)
-    "UNPARSED NODE - SHOULD NOT BE RENDERED!!!! #{content}"
+    def render_emphasisstar(content)
+      "*#{content}*"
+    end
+
+    def render_superscript(content)
+      "^#{content}"
+    end
+
+    def render_subscript(content)
+      "_#{content}"
+    end
+
+    def render_link(content, target, title)
+      if node.title.nil?
+        %Q{[#{content}](#{target})}
+      else
+        %Q{[#{content}](#{target} "#{title}")}
+      end
+    end
+
+    def render_reflink(content)
+      # TODO: Implement
+    end
+
+    def render_formula(content)
+      "\[ #{content} \]"
+    end
+
+    def render_deleted(content)
+      "~~#{content}~~"
+    end
+
+    def render_underline(content)
+      "~#{content}~"
+    end
+
+    def render_unparsed(content)
+      "UNPARSED NODE - SHOULD NOT BE RENDERED!!!! #{content}"
+    end
   end
 end
