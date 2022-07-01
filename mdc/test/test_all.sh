b4c2348a5f8a66b4e6d1ce25644f1fb4ac5beeed
Thomas Smits <thomsmits@gmail.com>
2022-07-01 19:13:41 +0200

    Better test script

--- mdc/test/test_all.sh
+++ mdc/test/test_all.sh
@@ -1,4 +1,17 @@
 #!/usr/bin/bash
 
-for file in ./*.rb; do ruby $file; done
+for file in ./*.rb; do
+  echo -e "\n\n--------------------------------------------"
+  echo "Executing: $file"
+  ruby "$file"
+  if [ $? -ne 0 ]
+    then
+      exit
+  fi
+done
+
+echo -e "\n\n"
+echo "==============================="
+echo "Finished without errors"
+echo "==============================="
 
