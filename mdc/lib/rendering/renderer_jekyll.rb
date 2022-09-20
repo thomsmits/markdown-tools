107d708551387937c707c1f4df262a43b99828f9
Thomas Smits <thomsmits@gmail.com>
2022-09-20 18:38:12 +0200

    Let Jekyll use KaTeX, too.

--- mdc/lib/rendering/renderer_jekyll.rb
+++ mdc/lib/rendering/renderer_jekyll.rb
@@ -87,8 +87,11 @@ module Rendering
           nav_order: <%= nav_order %>
           ---
         <%- if @has_equation -%>
-          <script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
-          <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
+          <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.16.2/dist/katex.min.css" integrity="sha384-bYdxxUwYipFNohQlHt0bjN/LCpueqWz13HufFEV1SUatKs1cm4L6fFgCi1jT643X" crossorigin="anonymous">
+          <script defer src="https://cdn.jsdelivr.net/npm/katex@0.16.2/dist/katex.min.js" integrity="sha384-Qsn9KnoKISj6dI8g7p1HBlNpVx0I8p1SvlwOldgi3IorMle61nQy4zEahWYtljaz" crossorigin="anonymous"></script>
+          <script defer src="https://cdn.jsdelivr.net/npm/katex@0.16.2/dist/contrib/auto-render.min.js" integrity="sha384-+VBxd3r6XgURycqtZ117nYw44OOcIax56Z4dCRWbxyPt0Koah1uHoK0o4+/RRE05" crossorigin="anonymous"
+            onload="renderMathInElement(document.body);">
+          </script>
         <%- end -%>
         <h1 class="fw-700 text-purple-300"><%= title %></h1>|
       ),
