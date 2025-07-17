ce865ca851ef1daf6cd7160f25255ffae39d8d27
Thomas Smits <thomsmits@gmail.com>
2025-07-17 23:27:16 +0200

    Made similar to other question classes

--- mdc/lib/domain/input_question.rb
+++ mdc/lib/domain/input_question.rb
@@ -9,11 +9,17 @@ module Domain
     ##
     # Create a new instance
     # @param [Array<String>] values possible answers
-    def initialize(values)
+    def initialize(values = [])
       super('')
       @values = values
     end
 
+    ##
+    # Add a value to the question.
+    # @param [String] the value to be added.
+    def <<(other)
+      @values << other
+    end
     ##
     # Render the element
     # @param [Rendering::Renderer] other Renderer to be used..
